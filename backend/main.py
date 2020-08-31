834a897a7d80d8e5bd5dcb966d21d216632726be
WTT <wilsonteng89@gmail.com>
2020-08-31 22:30:56 +0800

    Created config.py + Schema is now created on MySQL DB
    - Created config.py to store MySQL URI arguments.
    - import models to create schema in MySQL DB.

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 from flask import Flask
-from extensions import db
-from controllers.api import api
 
+import models  # Import models to create schema on MySQL DB.
+from config import *  # Flask App Config
+from controllers.api import api
+from extensions import db
+
+URI = 'mysql+pymysql://{0}:{1}@{2}:{3}/{4}'.format(USER, PASSWORD, HOST, PORT, SCHEMA)
 
 def register_extensions(app_obj):
     app_obj.app_context().push()
@@ -9,14 +13,12 @@ def register_extensions(app_obj):
     db.init_app(app_obj)
     db.create_all()
 
-
 def create_app(name):
     app_obj = Flask(name)
-    app_obj.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/ase'
+    app_obj.config['SQLALCHEMY_DATABASE_URI'] = URI # 'mysql+pymysql://root:toor@localhost:3306/ase'
     app_obj.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     register_extensions(app_obj)
-
     return app_obj
 
 
