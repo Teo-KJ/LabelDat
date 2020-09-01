45d4c474af8f2fabf3a05bc7ea4d42cd9fc4135d
jlgoh <gohxjl@gmail.com>
2020-09-01 16:13:04 +0800

    Add basic structure for integrating Formik and antd

--- frontend/src/components/labeller/LabelForm.js
+++ frontend/src/components/labeller/LabelForm.js
@@ -1,7 +1,65 @@
 import React from "react";
+import { Formik, Field, Form } from "formik";
+import { Radio } from "antd";
 
 const LabelForm = () => {
-  return <div>LabelForm</div>;
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <Formik
+        initialValues={{
+          picked: 3,
+        }}
+        onSubmit={async (values) => {
+          console.log(values);
+        }}
+      >
+        {({ values }) => (
+          <Form>
+            <div id="my-radio-group">Picked</div>
+            <div role="group" aria-labelledby="my-radio-group">
+              <Field
+                name="picked"
+                style={radioStyle}
+                type="radio"
+                as={Radio}
+                value={1}
+              >
+                Option A
+              </Field>
+              <Field
+                name="picked"
+                style={radioStyle}
+                type="radio"
+                as={Radio}
+                value={2}
+              >
+                Option B
+              </Field>
+              <Field
+                name="picked"
+                style={radioStyle}
+                type="radio"
+                as={Radio}
+                value={3}
+              >
+                Option C
+              </Field>
+              <div>Picked: {values.picked}</div>
+            </div>
+
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+const radioStyle = {
+  display: "block",
+  height: "30px",
+  lineHeight: "30px",
 };
 
 export default LabelForm;
