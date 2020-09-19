e881a6bda5a6ed4fed6b3d8039c0c01964a1d3c6
aryelciu001 <aryelciu001@gmail.com>
2020-09-20 01:35:10 +0800

    added upload files

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Dashboard from "./labeller/Dashboard";
-import LabelForm from "./labeller/LabelForm";
+import LabelForm from "./labeller/LabelForm/index";
 import TaskCreation from "./project-owner/TaskCreation";
 import SignIn from "./shared/SignIn";
 import SignUp from "./shared/SignUp";
+import UploadFiles from './project-owner/UploadFiles'
 
 function App() {
   // Insert routes here
@@ -14,6 +15,9 @@ function App() {
       <Route exact component={SignUp} path="/signup" />
       <Route exact component={SignIn} path="/signin" />
       <Route exact component={TaskCreation} path="/taskcreation" />
+      <Route exact path="/uploadfiles">
+        <UploadFiles dataType={"image"}></UploadFiles>
+      </Route>
       <Route exact component={LabelForm} path="/projects/:projectId/tasks" />
     </BrowserRouter>
   );
