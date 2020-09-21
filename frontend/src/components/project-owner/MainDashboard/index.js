af3e94925ad4098395e94ad5c72901fa3d15702a
jlgoh <gohxjl@gmail.com>
2020-09-21 13:27:24 +0800

    Setup PO Dashboard and mock API data

--- frontend/src/components/project-owner/MainDashboard/index.js
+++ frontend/src/components/project-owner/MainDashboard/index.js
@@ -1,32 +1,117 @@
-import React from "react";
-import { Typography, Divider, Row, Col, Card } from "antd";
-import { HomeOutlined } from "@ant-design/icons";
-import { Chart, Interval } from "bizcharts";
+import React, { Fragment } from "react";
+import { Typography, Divider, Table, Card } from "antd";
+import { Chart, Interval, Coordinate, Axis, Legend } from "bizcharts";
+import { Link } from "react-router-dom";
 import "./styles.scss";
 
 const data = [
-  { genre: "Sports", sold: 275 },
-  { genre: "Strategy", sold: 115 },
-  { genre: "Action", sold: 120 },
-  { genre: "Shooter", sold: 350 },
-  { genre: "Other", sold: 150 },
+  {
+    projectId: "projectA",
+    projectName: "Project A",
+    percentageLabelled: 30,
+    dateCreated: "25 Aug 2020",
+    tasksCount: 100,
+  },
+  {
+    projectId: "projectB",
+    projectName: "Project B",
+    percentageLabelled: 50,
+    dateCreated: "26 Aug 2020",
+    tasksCount: 200,
+  },
+  {
+    projectId: "projectC",
+    projectName: "Project C",
+    percentageLabelled: 70,
+    dateCreated: "27 Aug 2020",
+    tasksCount: 50,
+  },
+  {
+    projectId: "projectD",
+    projectName: "Project D",
+    percentageLabelled: 98,
+    dateCreated: "28 Aug 2020",
+    tasksCount: 20,
+  },
+  {
+    projectId: "projectE",
+    projectName: "Project E",
+    percentageLabelled: 5,
+    dateCreated: "29 Aug 2020",
+    tasksCount: 50,
+  },
 ];
 
-const style = { background: "#0092ff", padding: "8px 0" };
+const columns = [
+  {
+    title: "Project Name",
+    dataIndex: "projectName",
+    key: "projectName",
+    render: (text, record) => {
+      return <Link to={`/projects/${record.projectId}`}>{text}</Link>;
+    },
+  },
+  {
+    title: "Date Created",
+    dataIndex: "dateCreated",
+    key: "dateCreated",
+  },
+  {
+    title: "Total Number of Tasks",
+    dataIndex: "tasksCount",
+    key: "tasksCount",
+  },
+  {
+    title: "Percentage of Tasks Labelled",
+    dataIndex: "percentageLabelled",
+    key: "percentageLabelled",
+    render: (text) => <Fragment>{text}%</Fragment>,
+  },
+];
 
 const Dashboard = () => {
   return (
     <div className="dashboard-container">
       <Divider orientation="left">
-        <Typography.Title>
-          Dashboard <HomeOutlined />
-        </Typography.Title>
+        <Typography.Title>Dashboard</Typography.Title>
       </Divider>
 
-      <Card title="Chart Title" bordered={false} className="main-chart">
-        <Chart height={320} autoFit data={data}>
-          <Interval position="genre*sold" />
+      <Card
+        title={<b>Progress of Labelling for Your Projects</b>}
+        bordered={false}
+        className="main-chart"
+      >
+        <Chart
+          scale={{
+            projectName: { alias: "Project Name" },
+            percentageLabelled: { alias: "Percentage of Tasks Labelled (%)" },
+          }}
+          height={320}
+          autoFit
+          data={[...data].reverse()}
+        >
+          <Coordinate transpose />
+          <Axis title name="projectName" />
+          <Axis title name="percentageLabelled" />
+          <Interval
+            position="projectName*percentageLabelled"
+            color="projectName"
+          />
+          <Legend reversed offsetY={2} />
         </Chart>
+      </Card>
+
+      <Card
+        title={<b>Your Projects</b>}
+        bordered={false}
+        className="projects-table"
+      >
+        <Table
+          columns={columns}
+          dataSource={data}
+          size="small"
+          pagination={{ hideOnSinglePage: true }}
+        />
       </Card>
     </div>
   );
