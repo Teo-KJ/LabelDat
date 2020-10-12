a4b8a50c58ee49a5d8d0a15b6e7cd66232719f60
WTT <wilsonteng89@gmail.com>
2020-10-12 12:53:18 +0800

    Added numTasks in analytics route response

--- backend/utilities/analytics_response.py
+++ backend/utilities/analytics_response.py
@@ -1,12 +1,14 @@
 class AnalyticsResponse:
-    def __init__(self, project_name=None, overall_percentage=None, label_progress=None):
+    def __init__(self, project_name=None, num_tasks=None, overall_percentage=None, label_progress=None):
         self.project_name = project_name
+        self.num_tasks = num_tasks
         self.overall_percentage = overall_percentage
         self.label_progress = label_progress
 
     def to_dict(self):
         return {
             "projectName": self.project_name if self.project_name else "Project",
+            "numTasks": self.num_tasks,
             "overallPercentage": self.overall_percentage,
             "labelProgress": self.label_progress if self.label_progress else list()
         }
