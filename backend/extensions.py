ff18578c65605f52f47d3761d2cdeb3b0b13abe2
chiahsoon <chiahsoon18@gmail.com>
2020-10-18 02:25:34 +0800

    Added stuff for multi-threaded access to flask-sqlalchemy
    Signed-off-by: chiahsoon <chiahsoon18@gmail.com>

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,11 +1,31 @@
 import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
-
 from config import *
+from sqlalchemy.orm import sessionmaker,scoped_session
 
 url = 'mysql+pymysql://{0}:{1}@{2}'.format(USER, PASSWORD, HOST)
-engine = sqlalchemy.create_engine(url) # connect to server
-engine.execute("CREATE SCHEMA IF NOT EXISTS `ase`;") #create 'ase' schema if it does not exist
-engine.execute("USE ase;") # select new 'ase' schema
+engine = sqlalchemy.create_engine(url)  # connect to server
+engine.execute("CREATE SCHEMA IF NOT EXISTS `ase`;")    # create 'ase' schema if it does not exist
+engine.execute("USE ase;")  # select new 'ase' schema
 
 db = SQLAlchemy()
+session_factory = sessionmaker(bind=engine)
+Session = scoped_session(session_factory)
+
+
+# from sqlalchemy.orm import scoped_session
+# import threading
+# from models import *
+# from extensions import db, Session
+#
+# def run():
+#     threading.Thread(target=task).start()
+#     print("The main run function has returned")
+#     return
+#
+# def task():
+#     print(f"Task has started on {threading.current_thread().name}")
+#     sess = Session()
+#     print(f"Example query of users: {sess.query(User).filter_by(username='cs').first()}")
+#     print("Task has ended")
+#     Session.remove()
--- backend/utilities/__init__.py
+++ backend/utilities/__init__.py
@@ -4,7 +4,7 @@
 
 from utilities.generic_exception import GenericErrorResponse
 from utilities.rest_response import RestResponse
-from utilities.tasks_response import TasksAndLayoutResponse
+from utilities.tasks_and_layout_response import TasksAndLayoutResponse
 from utilities.user_projects_results import UserProjectsResults
 from utilities.analytics_response import AnalyticsResponse
 from utilities.file_response import FileResponseCSV
